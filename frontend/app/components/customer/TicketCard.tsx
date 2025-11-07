b869ac1536c8f18010899a7e3b35a892491fd8fb
liam-kristjanson <liam.kristjanson@gmail.com>
2025-11-07 14:14:21 +0100

    Show ticket qr code on customer portal

--- frontend/app/components/customer/TicketCard.tsx
+++ frontend/app/components/customer/TicketCard.tsx
@@ -1,4 +1,7 @@
-import { Button, Card } from "react-bootstrap";
+import { useState } from "react";
+import { Button, Card, Modal } from "react-bootstrap";
+import QRCode from "react-qr-code";
+import { Link } from "react-router";
 import type { Ticket } from "types";
 
 interface TicketCardProps {
@@ -6,6 +9,9 @@ interface TicketCardProps {
 }
 
 export default function TicketCard({ticket} : TicketCardProps) {
+
+    const [showTicketModal, setShowTicketModal] = useState<boolean>(false);
+
     return (
         <Card className="w-100">
             <Card.Img variant="top" src="/sample-concert.jpg" className="w-100"/>
@@ -26,11 +32,24 @@ export default function TicketCard({ticket} : TicketCardProps) {
                     
                 </Card.Text>
 
-                <Button variant="primary" className="me-1">Show Ticket</Button>
-                <Button variant="outline-dark">View event</Button>
+                <Button variant="primary" className="me-1" onClick={() => {setShowTicketModal(true)}}>Show Ticket</Button>
+                <Link to="event" state={{event: ticket.event}}><Button variant="outline-dark">View event</Button></Link>
             </Card.Body>
 
-            
+            <Modal show={showTicketModal} onHide={() => {setShowTicketModal(false)}}>
+                <Modal.Header closeButton>
+                    <Modal.Title>
+                        Ticket for {ticket.event.title}
+                    </Modal.Title>
+                </Modal.Header>
+
+                <Modal.Body>
+                    <QRCode value={ticket._id}/>
+
+                    <br/>
+                    {ticket.event.eventLocation}
+                </Modal.Body>
+            </Modal>
         </Card>
     )
 }
