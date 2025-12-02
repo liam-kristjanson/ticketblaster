bd62ed1f67fd6a025960bc00ecd4a243d4105395
liam-kristjanson <liam.kristjanson@gmail.com>
2025-12-02 12:36:47 +0100

    Feature for hosts to delete events

--- backend/src/controllers/eventController.ts
+++ backend/src/controllers/eventController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express"
 import Event from "../models/Event";
 import { ObjectId } from "mongodb";
+import Ticket from "../models/Ticket";
 
 export async function createEvent(req: Request, res: Response) {
 
@@ -50,23 +51,54 @@ export async function getEvents(req: Request, res: Response) {
 
 export async function deleteEvent(req: Request, res: Response) {
     if (req.user?.role === "admin") {
-
-        if (!req.query.eventId) {
-            res.status(400).json({error: "eventId must be specified in querystring."});
-            return;
-        }
-
-        const result = await Event.deleteOne({_id: req.query.eventId}).exec();
-
-        if (result.deletedCount === 0) {
-            res.status(404).json({error: "Event with given eventId not found"});
-            return;
-        }
-
-        res.status(200).json({message: "Event deleted successfuly"});
-
+        adminDeleteEvent(req, res);
+    } else if (req.user?.role === "host") {
+        hostDeleteEvent(req, res);
     } else {
         res.status(401).json({error: "400: Unauthorized"});
+    }
+}
+
+async function adminDeleteEvent(req: Request, res: Response) {
+    if (!req.query.eventId) {
+        res.status(400).json({error: "eventId must be specified in querystring."});
+        return;
+    }
+
+    const result = await Event.deleteOne({_id: req.query.eventId}).exec();
+
+    if (result.deletedCount === 0) {
+        res.status(404).json({error: "Event with given eventId not found"});
+        return;
+    }
+
+    res.status(200).json({message: "Event deleted successfuly"});
+}
+
+async function hostDeleteEvent(req: Request, res: Response) {
+    if (!("string" === typeof req.query?.eventId)) {
+        res.status(400).json({error: "eventId must be specified in querystring."});
+    }
+
+    try {
+        const matchedEvent = await Event.findById(req.query.eventId);
+
+        if (!matchedEvent || req.user?.id != matchedEvent.owner.toString()) {
+            res.status(404).json({error: "invalid eventId"});
+            return;
+        }
+
+        console.log("Deleting tickets for event with id" + req.query.eventId);
+        const ticketDeletionResult = await Ticket.deleteMany({event: matchedEvent._id});
+        console.log("Deleted " + ticketDeletionResult.deletedCount + " ticket(s).");
+
+        console.log("Deleting event document with id " + matchedEvent._id);
+        const eventDeleteResult = await matchedEvent.deleteOne();
+        console.log("Deleted " + eventDeleteResult.deletedCount + " event(s).");
+
+        res.json({message: "Event deleted successfuly."})
+    } catch (err) {
+        console.error("The following error occured while deleting event with id " + req.query.eventId, err);
     }
 }
 
--- frontend/app/routes/host/eventDashboard.tsx
+++ frontend/app/routes/host/eventDashboard.tsx
@@ -1,7 +1,8 @@
 import { useContext, useEffect, useState } from "react";
-import { Container, Spinner, Table } from "react-bootstrap";
-import { Navigate, useLocation } from "react-router"
+import { Button, Container, Modal, Spinner, Table } from "react-bootstrap";
+import { Navigate, useLocation, useNavigate } from "react-router"
 import type { ServerMessage, Ticket, TicketEvent } from "types";
+import ServerResponseContainer from "~/components/ServerResopnseContainer";
 import { AuthContext } from "~/context/authContext";
 
 export default function EventDashboard() {
@@ -12,12 +13,49 @@ export default function EventDashboard() {
         <h1>Error loading event details! Please try again</h1>
     </>
 
+    const navigate = useNavigate();
+
     const { user } = useContext(AuthContext);
     if (!user) return <Navigate to="/login"/>
 
     const [tickets, setTickets] = useState<Ticket[]>([]);
     const [ticketsLoading, setTicketsLoading] = useState<boolean>(false);
     const [ticketLoadMsg, setTicketLoadMsg] = useState<ServerMessage>(['', 'info']);
+
+    const [showDeleteModal, setShowDeleteModal] = useState<boolean>(false);
+    const [isEventDeleting, setIsEventDeleting] = useState<boolean>(false);
+    const [eventDeletionMsg, setEventDeletionMsg] = useState<ServerMessage>(['', 'info'])
+
+    function handleDeleteEvent() {
+        setIsEventDeleting(true);
+        setEventDeletionMsg(['', 'info']);
+
+        fetch(import.meta.env.VITE_SERVER + "/event?eventId=" + event._id, {
+            method: "DELETE",
+            headers: {
+                Authorization: user?.authToken ?? ""
+            }
+        })
+        .then(response => {
+            response.json().then(responseJson => {
+                if (response.ok) {
+                    setEventDeletionMsg([responseJson.message ?? "Success", "success"]);
+                    setTimeout(() => {
+                        navigate('/host');
+                    }, 3000)
+                } else {
+                    setEventDeletionMsg([responseJson.error ?? "An error occured while deleting event.", 'danger']);
+                }
+            })
+        })
+        .catch(err => {
+            console.error(err);
+            setEventDeletionMsg(["An unexpected error occured while deleting event", "danger"]);
+        })
+        .finally(() => {
+            setIsEventDeleting(false);
+        })
+    }
 
     useEffect(() => {
         setTicketsLoading(true);
@@ -101,6 +139,58 @@ export default function EventDashboard() {
                     ))}
                 </tbody>
             </Table>
+
+            <hr/>
+
+            <h3>Event Management</h3>
+
+            <Button className="mb-5" onClick={() => {setShowDeleteModal(true)}} size={"lg"} variant="danger">Delete Event</Button>
         </Container>
+
+        <Modal show={showDeleteModal} onHide={() => {setShowDeleteModal(false)}}>
+            <Modal.Header closeButton>
+                <Modal.Title>Delete {event.title}?</Modal.Title>
+            </Modal.Header>
+
+            <Modal.Body>
+                <p>
+                    Are you sure you want to delete this event? This action cannot be undone. All created tickets will also be deleted.
+                </p>
+
+                <h5>Event Details</h5>
+                <hr/>
+
+                <p>
+                    Title: {event.title}
+                </p>
+
+                <p>
+                    Venue: {event.venue.name}
+                </p>
+
+                <p>
+                    Date: {new Date(event.startTime).toLocaleDateString()}
+                </p>
+
+                <p>
+                    Time: {new Date(event.startTime).toLocaleTimeString()}
+                </p>
+            </Modal.Body>
+
+            <Modal.Footer>
+                {isEventDeleting ? (
+                    <>
+                        <Spinner/> Deleting event...
+                    </>
+                ) : (
+                    <>
+                        <Button variant="secondary">Cancel</Button>
+                        <Button variant="danger" onClick={() => {handleDeleteEvent()}}>Delete {event.title}</Button>
+                    </>
+                )}
+
+                <ServerResponseContainer response={eventDeletionMsg}/>
+            </Modal.Footer>
+        </Modal>
     </>
 }
